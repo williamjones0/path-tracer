fe89d74cb40337581108adb53d66c0eab842d277
William Jones <wmjones3142@gmail.com>
2024-07-30 23:39:06 +0100

    Add Cornell box scene

--- hittable.h
+++ hittable.h
@@ -28,3 +28,104 @@ public:
 
 	virtual aabb bounding_box() const = 0;
 };
+
+class translate : public hittable {
+public:
+	translate(shared_ptr<hittable> object, const vec3 &offset) : object(object), offset(offset) {
+		bbox = object->bounding_box() + offset;
+	}
+
+	bool hit(const ray &r, interval ray_t, hit_record &rec) const override {
+		ray offset_r(r.origin() - offset, r.direction(), r.time());
+
+		if (!object->hit(offset_r, ray_t, rec))
+			return false;
+
+		rec.p += offset;
+
+		return true;
+	}
+
+	aabb bounding_box() const override {
+		return bbox;
+	}
+
+private:
+	shared_ptr<hittable> object;
+	vec3 offset;
+	aabb bbox;
+};
+
+class rotate_y : public hittable {
+public:
+	rotate_y(shared_ptr<hittable> object, double angle) : object(object) {
+		auto radians = degrees_to_radians(angle);
+		sin_theta = std::sin(radians);
+		cos_theta = std::cos(radians);
+		bbox = object->bounding_box();
+
+		point3 min(infinity, infinity, infinity);
+		point3 max(-infinity, -infinity, -infinity);
+
+		for (int i = 0; i < 2; i++) {
+			for (int j = 0; j < 2; j++) {
+				for (int k = 0; k < 2; k++) {
+					auto x = i * bbox.x.max + (1 - i) * bbox.x.min;
+					auto y = j * bbox.y.max + (1 - j) * bbox.y.min;
+					auto z = k * bbox.z.max + (1 - k) * bbox.z.min;
+
+					auto newx = cos_theta * x + sin_theta * z;
+					auto newz = -sin_theta * x + cos_theta * z;
+
+					vec3 tester(newx, y, newz);
+
+					for (int c = 0; c < 3; c++) {
+						min[c] = std::fmin(min[c], tester[c]);
+						max[c] = std::fmax(max[c], tester[c]);
+					}
+				}
+			}
+		}
+
+		bbox = aabb(min, max);
+	}
+
+	bool hit(const ray &r, interval ray_t, hit_record &rec) const override {
+		auto origin = r.origin();
+		auto direction = r.direction();
+
+		origin[0] = cos_theta * r.origin()[0] - sin_theta * r.origin()[2];
+		origin[2] = sin_theta * r.origin()[0] + cos_theta * r.origin()[2];
+
+		direction[0] = cos_theta * r.direction()[0] - sin_theta * r.direction()[2];
+		direction[2] = sin_theta * r.direction()[0] + cos_theta * r.direction()[2];
+
+		ray rotated_r(origin, direction, r.time());
+
+		if (!object->hit(rotated_r, ray_t, rec))
+			return false;
+
+		auto p = rec.p;
+		p[0] = cos_theta * rec.p[0] + sin_theta * rec.p[2];
+		p[2] = -sin_theta * rec.p[0] + cos_theta * rec.p[2];
+
+		auto normal = rec.normal;
+		normal[0] = cos_theta * rec.normal[0] + sin_theta * rec.normal[2];
+		normal[2] = -sin_theta * rec.normal[0] + cos_theta * rec.normal[2];
+
+		rec.p = p;
+		rec.normal = normal;
+
+		return true;
+	}
+
+	aabb bounding_box() const override {
+		return bbox;
+	}
+
+private:
+	shared_ptr<hittable> object;
+	double sin_theta;
+	double cos_theta;
+	aabb bbox;
+};
--- interval.h
+++ interval.h
@@ -41,3 +41,11 @@ public:
 
 const interval interval::empty = interval(+infinity, -infinity);
 const interval interval::universe = interval(-infinity, +infinity);
+
+interval operator+(const interval &ival, double displacement) {
+	return interval(ival.min + displacement, ival.max + displacement);
+}
+
+interval operator+(double displacement, const interval &ival) {
+	return ival + displacement;
+}
